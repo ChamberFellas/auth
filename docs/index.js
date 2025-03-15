025f94fbaf147fd856dea1df7675bf2aac24d5e0
FinnMCG <85682523+finn-485768594@users.noreply.github.com>
2025-03-15 12:31:37 +0000

    Update index.js
    Signed-off-by: FinnMCG <85682523+finn-485768594@users.noreply.github.com>

--- docs/index.js
+++ docs/index.js
@@ -67,3 +67,69 @@ app.get('/auth/internal/refresh',(req,res) => {
 
     
 });
+
+//registering a new user
+app.post('/auth/register',(req,res) => {
+    const {username} = req.body;
+    const {password} = req.body;
+    
+    
+    /* 
+    "username": "bob"
+    "password" : "bobsPassword"
+    */
+
+    if(!username || !password){
+        res.status(418).send({
+            error: 'you didnt send all the data silly!'
+        })
+    }else {
+        res.status(200).send({
+            username: 'you succesfully sent the username : '+username,
+            password: password,
+            result: 'succesfully added to the totally real database'
+        })
+    }
+    /*else{
+
+        res.status(401).send({
+            error: 'Invalid details'
+        }) 
+    }*/
+    // the above will neeed to be included but as i sont currently have anything to validate if a password and username is acceptable i had to comment it out for now
+
+    
+});
+
+
+//loging in a user
+app.get('/auth/login',(req,res) => {
+    const {username} = req.body;
+    const {password} = req.body;
+    
+    //tempoary variables to test endpoints
+    const validUsername = 'bob';
+    const validPassword = 'bobsPassword';
+    const newTokenAccess = 'ABCD1';
+    const newTokenRefresh = 'abcd2';
+
+    if(!username || !password){
+        res.status(418).send({
+            error: 'you didnt send all the data silly!'
+        })
+    }else if((username == validUsername)&(password == validPassword)) {
+        res.status(200).send({
+            username: 'you succesfully sent the username : '+username,
+            password: password,
+            accessToken : newTokenAccess,
+            refreahToken : newTokenRefresh
+            
+        })
+    }else{
+
+        res.status(401).send({
+            error: 'Invalid details'
+        }) 
+    }
+    
+});
