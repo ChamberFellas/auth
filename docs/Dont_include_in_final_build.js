84a38faed9e43a29a4c10290af2c752d1713dbae
FinnMCG <85682523+finn-485768594@users.noreply.github.com>
2025-03-18 15:18:03 +0000

    functions to connect
    these are the exact functions you should use for:
    >login
    >register
    >validation
    
    other functions to come but just copy and paste them int
    
    Signed-off-by: FinnMCG <85682523+finn-485768594@users.noreply.github.com>

--- docs/Dont_include_in_final_build.js
+++ docs/Dont_include_in_final_build.js
@@ -1,13 +1,17 @@
 // client.js
 const axios = require('axios');
+const bcrypt = require("bcryptjs");
+const crypto = require('crypto');
 
 // Make requests to your endpoints like so
 
 // Example: Call the /auth/register endpoint
-async function registerUser() {
+async function registerUser(username,password) {
+
+    const hashedPassword = crypto.createHash('sha256').update((password+username)).digest('hex')
     const userData = {
-        username: "DAVE",
-        password: "DAVESsecurepassword"
+        username: username,
+        password: hashedPassword
     };
 
     try {
@@ -19,20 +23,34 @@ async function registerUser() {
 }
 
 // Example: Call the /auth/login endpoint
-async function loginUser() {
+async function loginUser(username,password){
+    const hashedPassword = crypto.createHash('sha256').update((password+username)).digest('hex')
     const userData = {
-        username: "DAVE",
-        password: "DAVESsecurepassword"
+        username: username,
+        password: hashedPassword
     };
 
     try {
         const response = await axios.post('http://localhost:8080/auth/login', userData);
         console.log(response.data);
     } catch (error) {
-        console.error('Error logging in:', error.response ? error.response.data : error.message);
+        console.error('Error logging in:2', error.response ? error.response.data : error.message);
     }
 }
 
+// Example: Call the /auth/internal/validate endpoint to validate a token
+async function validateToken(token) {
+    try {
+        const response = await axios.post('http://localhost:8080/auth/internal/validate', { token });
+        console.log("Token Validation Response:", response.data);
+    } catch (error) {
+        console.error('Error validating token:', error.response ? error.response.data : error.message);
+    }
+}
+
+
+
 // Run a test
-//registerUser();
-//loginUser();
+//registerUser("finn","password");//create boolean for success
+//loginUser("finn","password");
+validateToken('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiZmlubiIsImlhdCI6MTc0MjMxMDkyOCwiZXhwIjoxNzQyMzExNTI4fQ.7DhMjQxlyDZKabxmnrTiLPOYf6ZccIXxKIcVvuK9ieM');
