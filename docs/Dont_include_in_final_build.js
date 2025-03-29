30bdb548391ad6dce9e12f865e61c39ecfcca757
FinnMCG <85682523+finn-485768594@users.noreply.github.com>
2025-03-29 18:23:00 +0000

    Update Dont_include_in_final_build.js
    Signed-off-by: FinnMCG <85682523+finn-485768594@users.noreply.github.com>

--- docs/Dont_include_in_final_build.js
+++ docs/Dont_include_in_final_build.js
@@ -1,9 +1,8 @@
 // client.js
-const axios = require('axios');
-const bcrypt = require("bcryptjs");
-const crypto = require('crypto');
-
+import axios from 'axios';
+import crypto from 'crypto';
 // Make requests to your endpoints like so
+const host= '172.26.107.70:8080';
 
 // Example: Call the /auth/register endpoint
 async function registerUser(username,password) {
@@ -15,10 +14,11 @@ async function registerUser(username,password) {
     };
 
     try {
-        const response = await axios.post('http://localhost:8080/auth/register', userData);
-        console.log(response.data);
+        const response = await axios.post('http://'+host+'/auth/register', userData);
+        //console.log(response.data);
+        return [response.data.message,response.data.username];
     } catch (error) {
-        console.error('Error registering user:', error.response ? error.response.data : error.message);
+        return [('Error updating account:', error.response ? error.response.data : error.message),0];
     }
 }
 
@@ -31,30 +31,31 @@ async function loginUser(username,password){
     };
 
     try {
-        const response = await axios.post('http://localhost:8080/auth/login', userData);
-        console.log(response.data);
+        const response = await axios.post('http://'+host+'/auth/login', userData);
+        return ['success',response.data.access_token];
     } catch (error) {
-        console.error('Error logging in:', error.response ? error.response.data : error.message);
+        return [('Error updating account:', error.response ? error.response.data : error.message),0];
     }
 }
 
 // Example: Call the /auth/internal/validate endpoint to validate a token
 async function validateToken(token) {
     try {
-        const response = await axios.post('http://localhost:8080/auth/internal/validate', { token });
-        console.log("Token Validation Response:", response.data);
+        const response = await axios.post('http://'+host+'/auth/internal/validate', { token });
+        return [response.data.message,response.data.userID,response.data.ValidID];
+        //console.log("Token Validation Response:", response.data);
     } catch (error) {
-        console.error('Error validating token:', error.response ? error.response.data : error.message);
+        return [('Error updating account:', error.response ? error.response.data : error.message),0,0];
     }
 }
 
 async function deleteAccount(token,username,password) {
     const hashedPassword = crypto.createHash('sha256').update((password+username)).digest('hex')
     try {
-        const response = await axios.post('http://localhost:8080/auth/delete', { token, hashedPassword });
-        console.log("account Deleting Response:", response.data);
+        const response = await axios.post('http://'+host+'/auth/delete', { token, hashedPassword });
+        return [response.data.message,response.data.userID_DELETED];
     } catch (error) {
-        console.error('Error deleting account:', error.response ? error.response.data : error.message);
+        return [('Error updating account:', error.response ? error.response.data : error.message),0];
     }
 }
 
@@ -63,18 +64,27 @@ async function updateAccount(token,newerpassword,username,olderpassword) {
     const oldPassword = crypto.createHash('sha256').update((olderpassword+username)).digest('hex')
     const newPassword = crypto.createHash('sha256').update((newerpassword+username)).digest('hex')
     try {
-        const response = await axios.post('http://localhost:8080/auth/update', { token, oldPassword, newPassword });
-        console.log("account updating Response:", response.data);
+        const response = await axios.post('http://'+host+'/auth/update', { token, oldPassword, newPassword });
+        return [response.data.message,response.data.userID_UPDATED];
     } catch (error) {
-        console.error('Error updating account:', error.response ? error.response.data : error.message);
+        return [('Error updating account:', error.response ? error.response.data : error.message),0];
     }
 }
 
 
 
 // Run a test
-//registerUser("test","password");//create boolean for success
-//loginUser("test","password");
-//validateToken('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoidGVzdCIsImlhdCI6MTc0MjMxODA1MiwiZXhwIjoxNzQyMzE4NjUyfQ.wy8_rTTv0RDju7ARAW0t2no3aurdTFEmQmhfWAgyBsY');
-//deleteAccount('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoidGVzdCIsImlhdCI6MTc0MjMyMDU3NCwiZXhwIjoxNzQyMzIxMTc0fQ.CiK37TJ-qo2lU5IkrAcszbyEXw7cM-rkpITMjR9iPaQ','test','password');
-//updateAccount('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoidGVzdCIsImlhdCI6MTc0MjMxOTA5OCwiZXhwIjoxNzQyMzE5Njk4fQ.LxokwlSg_MCLE_Utrz6k-h8YPoMwtqFyYNPcK9oU6Zs','secondPassword','test','password');         
+const outputTESTregister = await registerUser("test", "password");
+console.log(outputTESTregister); // no need to await console.log
+
+const outputTESTlogin = await loginUser("test","password");
+console.log(outputTESTlogin);
+
+const outputTESTvalidate = await validateToken('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoidGVzdCIsImlhdCI6MTc0MjMxODA1MiwiZXhwIjoxNzQyMzE4NjUyfQ.wy8_rTTv0RDju7ARAW0t2no3aurdTFEmQmhfWAgyBsY');
+console.log(outputTESTvalidate);
+
+const outputTESTdelete = await deleteAccount('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoidGVzdCIsImlhdCI6MTc0MjMyMDU3NCwiZXhwIjoxNzQyMzIxMTc0fQ.CiK37TJ-qo2lU5IkrAcszbyEXw7cM-rkpITMjR9iPaQ','test','password');
+console.log(outputTESTdelete);
+
+const outputTESTupdate = await updateAccount('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoidGVzdCIsImlhdCI6MTc0MjMxOTA5OCwiZXhwIjoxNzQyMzE5Njk4fQ.LxokwlSg_MCLE_Utrz6k-h8YPoMwtqFyYNPcK9oU6Zs','secondPassword','test','password');         
+console.log(outputTESTupdate);
